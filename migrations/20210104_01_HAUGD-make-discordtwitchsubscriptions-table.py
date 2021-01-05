968140d31321095626b66416a408458d354becd1
arybutler <andrew.ry.butler@gmail.com>
2021-01-05 03:29:43 -0800

    Fix migration

--- migrations/20210104_01_HAUGD-make-discordtwitchsubscriptions-table.py
+++ migrations/20210104_01_HAUGD-make-discordtwitchsubscriptions-table.py
@@ -7,5 +7,5 @@ from yoyo import step
 __depends__ = {}
 
 steps = [
-    step("CREATE TABLE discordTwitchSubscriptions (streamerId TEXT, guildId TEXT, channelId TEXT, roleId TEXT, message TEXT, PRIMARY KEY (streamerId, guildId));"),
+    step("CREATE TABLE discordTwitchSubscriptions (streamerId VARCHAR(50), guildId VARCHAR(50), channelId VARCHAR(50), roleId VARCHAR(50), message VARCHAR(50), PRIMARY KEY (streamerId, guildId));"),
 ]
